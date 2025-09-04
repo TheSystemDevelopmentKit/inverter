dae2f7e46129278d3462c4622badd7b28cc1ad58
Marko Kosunen <marko.kosunen@aalto.fi>
2025-09-04 17:01:31 +0300

    Add init_submodules.sh to recursively configure pre-commit hooks

--- inverter/__init__.py
+++ inverter/__init__.py
@@ -32,10 +32,6 @@ defined for the inverter.
 
 """
 
-
-
-
-
 import os
 import sys
 
@@ -48,12 +44,6 @@ from spice import spice, spice_iofile, spice_dcsource, spice_simcmd
 
 
 import numpy as np
-
-
-
-
-
-
 
 
 class inverter(rtl, spice, thesdk):
